1150bc72edfd5daa714d561c0179f988edfc7e66
CrossTech <526383173@qq.com>
2024-11-03 19:02:25 +0800

    Implement prediction panel

--- dashboard.js
+++ dashboard.js
@@ -9,9 +9,7 @@ Funbit.Ets.Telemetry.Dashboard.prototype.initialize = function (skinConfig, util
     // this function is called before everything else, 
     // so you may perform any DOM or resource initializations / image preloading here
 
-    utils.preloadImages([
-
-    ]);
+    utils.preloadImages([]);
 
     // return to menu by a click
     /*
@@ -67,13 +65,54 @@ Funbit.Ets.Telemetry.Dashboard.prototype.filter = function (data, utils) {
     // return changed data to the core for rendering
 
     // Own data
-    const remainingDate = new Date(data.job.remainingTime)
-    data.job.remainingTime =
-        utils.formatInteger(remainingDate.getDate(), 2) + " d " +
-        utils.formatInteger(remainingDate.getHours(), 2) + ":" +
-        utils.formatInteger(remainingDate.getMinutes(), 2) + "";
     const now = new Date();
     data.clock = utils.formatInteger(now.getHours(), 2) + ":" + utils.formatInteger(now.getMinutes(), 2);
+    if (data.game.connected) {
+        data.fuelBarLength = data.truck.fuel / data.truck.fuelCapacity * 452 + "px";
+        data.fuelPercentage = Math.round(data.truck.fuel / data.truck.fuelCapacity * 100) + "%";
+        data.fob = Math.round(data.truck.fuel);
+        data.fuelCapacity = Math.round(data.truck.fuelCapacity);
+        data.fuelConsump = utils.formatFloat(data.truck.fuelAverageConsumption, 2)
+        data.fuelRange = utils.formatFloat(data.truck.fuel / data.truck.fuelAverageConsumption, 2)
+        if (data.fuelRange >= 10000) data.fuelRange = 9999.99;
+
+        const now = new Date();
+        if (data.game.timeScale > 10) {
+            let remTime = 0;
+            if (data.truck.cruiseControlOn) {
+                remTime =
+                    data.navigation.estimatedDistance / (data.truck.cruiseControlSpeed / 3.6) / data.game.timeScale;
+            } else {
+                remTime =
+                    data.navigation.estimatedDistance / (data.truck.cruiseControlSpeed / 3.6) / data.game.timeScale;
+            }
+            if (remTime >= 99 * 3600) {
+                remTime = 99 * 3600;
+            }
+
+            console.log(new Date(remTime * 1000).toISOString())
+            data.ete = new Date(remTime * 1000).toISOString().substring(11, 19);
+
+            const eta = new Date(now.setSeconds(remTime * 1.5));
+            data.eta = utils.formatInteger(eta.getHours(), 2) + ":" + utils.formatInteger(eta.getMinutes(), 2);
+        } else {
+            data.ete = "00:00:00"
+            data.eta = data.clock;
+        }
+    } else {
+        data.ete = "00:00:00"
+        data.eta = data.clock;
+    }
+
+    data.gameSpeed = "x" + data.game.timeScale;
+
+    const remainingDate = new Date(data.job.remainingTime)
+    data.job.remainingTime = data.game.connected ?
+        utils.formatInteger(remainingDate.getDate(), 2) + " d " +
+        utils.formatInteger(remainingDate.getHours(), 2) + ":" +
+        utils.formatInteger(remainingDate.getMinutes(), 2) + ""
+        :
+        "-";
     const remainDist = data.navigation.estimatedDistance;
     const distInt = Math.floor(remainDist / 1000);
     data.distanceInteger = distInt;
@@ -81,7 +120,7 @@ Funbit.Ets.Telemetry.Dashboard.prototype.filter = function (data, utils) {
     data.speedPos = -1080 + data.truck.speed * 6.20;
     // speed trend
     const accelVct = data.truck.acceleration;
-    const accel = Math.sqrt(accelVct.x**2 + accelVct.y**2 + accelVct.z**2);
+    const accel = Math.sqrt(accelVct.x ** 2 + accelVct.y ** 2 + accelVct.z ** 2);
     let spdTrendDelta = accel * 10;// speed trend after 10 sec
     if (data.truck.speed <= historySpd - 0.05) spdTrendDelta = -spdTrendDelta;
     data.ledCount = Math.max(Math.min(Math.round(spdTrendDelta / 2.5), 11), -19);
@@ -116,11 +155,14 @@ Funbit.Ets.Telemetry.Dashboard.prototype.render = function (data, utils) {
     const compass = document.getElementById("compass");
     compass.style.backgroundPosition = data.compassPos + "px 50%";
 
+    const fuelBar = document.getElementById("fuel-bar");
+    fuelBar.style.width = data.fuelBarLength;
+
     //Odometer
     const odo = data.truck.odometer;
     const digDec = document.getElementById("odometer-decimal");
     const odoDec = Math.floor((odo - Math.floor(odo)) * 1000);
-    digDec.style.backgroundPosition = "50% " + getDecimalOffset(odoDec, 100, 10.1/9) + "%";
+    digDec.style.backgroundPosition = "50% " + getDecimalOffset(odoDec, 100, 10.1 / 9) + "%";
     for (let i = 0; i < 6; i++) {
         const digitElem = document.getElementById("odometer-digit" + i);
         const intPart = Math.floor(odo);
@@ -130,35 +172,34 @@ Funbit.Ets.Telemetry.Dashboard.prototype.render = function (data, utils) {
         const fullString = utils.formatInteger(Math.floor(odo), 6) + "." + utils.formatInteger(odoDec, 3);
         const remainder = fullString.toString().substring(i + 1);
         const remainderFloat = parseFloat(remainder)
-        console.log(odo, remainder, remainderFloat)
+        //console.log(odo, remainder, remainderFloat)
         const offset = getDigitOffset(
             parseInt(currentDigit),
             nextDigit,
             remainderFloat,
             100,
-            10.1/9) ;
+            10.1 / 9);
         //console.log(i, intPart, workingPart, currentDigit, remainder, offset)
         digitElem.style.backgroundPosition = "50% " + offset + "%";
     }
 }
 
-function len(num){
-    return Math.ceil(Math.log10(num+1))
+function len(num) {
+    return Math.ceil(Math.log10(num + 1))
 }
 
-function getDecimalOffset(deci, height, coefficient){
+function getDecimalOffset(deci, height, coefficient) {
     return deci / 10 ** 3 * height * coefficient;
 }
 
-function getDigitOffset(curr, next, rem, height, coefficient){
+function getDigitOffset(curr, next, rem, height, coefficient) {
     if (10 ** len(Math.floor(rem)) - rem < 0.1 && next === 9) {
         return ((curr + 1) / 10 + rem - 10 ** len(Math.floor(rem))) * height * coefficient;
-    }
-    else {
+    } else {
         return curr / 10 * height * coefficient;
     }
 }
 
-function getIntegrity(wear){
-    return (100 -Math.round(wear * 100) + "%")
+function getIntegrity(wear) {
+    return (100 - Math.round(wear * 100) + "%")
 }
